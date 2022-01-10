5985028a82c50a5de7609f8e409444a344b8d2f6
Khaya Ubisi <nettcaster87@gmail.com>
2022-01-10 18:25:13 +0200

    Commit chapter 1 exercises

---
+++ Crash Course/Basics/Chapter1/try_it_yourself.py
@@ -0,0 +1,14 @@
+# ### 1.1 Search 
+# Search the python website to find topics of interest
+
+# ### 1.2 Hello world typos
+# print("Hello World! yes")
+
+# ### 1.3 Infinite skills
+"""
+Possible future projects
+- Day trading algorithm
+- Web scrapping app
+- Automation suite (megatron)
+- 
+"""
